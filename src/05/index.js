7a0cf001aa67632a2a188d03d5217f147125125c
Mohammed Khatana <scm@mohammedkhatana.com>
2020-12-05 21:37:13 +0000

    binary conversion worked as well for day 5 answer

--- src/05/index.js
+++ src/05/index.js
@@ -1,37 +1,20 @@
-const decode = (data, top, bottom, upHalfChar, lowHalfChar) => {
-  var max = top;
-  var min = bottom;
-
-  for (let index = 0; index < data.length; index++) {
-    const element = data[index];
-
-    const UpperHalf = Math.floor(((max + min) / 2));
-    const LowerHalf = Math.ceil(max - ((max - min) / 2));
-    max = element === upHalfChar ? UpperHalf : max;
-    min = element === lowHalfChar ? LowerHalf : min;
-    // console.debug(`char = ${element}, max = ${max}, min = ${min}`)
-  }
-  // console.debug(`final values = ${max} ${min}`);
-  return max;
-}
-
-export const decodeData = (input) => {
+const decodeAllData = (input) => {
   const rowData = input.slice(0, 7);
   const columnData = input.slice(7);
 
-  const row = decode(rowData, 127, 0, 'F', 'B');
-  const column = decode(columnData, 7, 0, 'L', 'R');
+  const row = parseInt(rowData.replace(/F/g, 0).replace(/B/g, 1), 2);
+  const column = parseInt(columnData.replace(/L/g, 0).replace(/R/g, 1), 2);
   return { row, column, seat: (row * 8) + column }
 }
 
 
 const getAllSeats = (input) =>
-  input.map(e => decodeData(e).seat).sort((a, b) => b - a);
+  input.map(e => decodeAllData(e).seat).sort((a, b) => b - a);
 
-export const maxSeatId = (input) => getAllSeats(input)[0]
+const maxSeatId = (input) => getAllSeats(input)[0]
 
 
-export const findMissingSeat = (input) => {
+const findMissingSeat = (input) => {
   const seats = getAllSeats(input);
 
   let possibleSeats = [];
@@ -44,7 +27,12 @@ export const findMissingSeat = (input) => {
       possibleSeats.push(element);
     }
   }
-  
+
   return possibleSeats.reduce((a, b) => (a + b) / 2);
 
+}
+
+
+module.exports = {
+  findMissingSeat, decodeAllData, maxSeatId
 }
--- src/05/index.test.js
+++ src/05/index.test.js
@@ -1,4 +1,4 @@
-import { decodeData, maxSeatId, findMissingSeat } from "./index";
+import { decodeAllData, maxSeatId, findMissingSeat } from "./index";
 import puzzle from "./puzzle";
 
 
@@ -6,7 +6,7 @@ describe("part 1", () => {
   test("should return row 44, column 5, seat id 357", () => {
     const input = `FBFBBFFRLR`;
 
-    const decoded = decodeData(input);
+    const decoded = decodeAllData(input);
 
     expect(decoded.row).toBe(44);
     expect(decoded.column).toBe(5);
@@ -14,7 +14,7 @@ describe("part 1", () => {
   });
   test("should return row 70, column 7, seat id 567", () => {
     const input = `BFFFBBFRRR`;
-    const decoded = decodeData(input);
+    const decoded = decodeAllData(input);
 
     expect(decoded.row).toBe(70);
     expect(decoded.column).toBe(7);
@@ -22,7 +22,7 @@ describe("part 1", () => {
   });
   test("should return row 14, column 7, seat id 119", () => {
     const input = `FFFBBBFRRR`;
-    const decoded = decodeData(input);
+    const decoded = decodeAllData(input);
 
     expect(decoded.row).toBe(14);
     expect(decoded.column).toBe(7);
@@ -30,7 +30,7 @@ describe("part 1", () => {
   });
   test("should return row 102, column 4, seat id 820", () => {
     const input = `BBFFBBFRLL`;
-    const decoded = decodeData(input);
+    const decoded = decodeAllData(input);
 
     expect(decoded.row).toBe(102);
     expect(decoded.column).toBe(4);
